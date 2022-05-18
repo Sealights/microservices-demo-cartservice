5f5821c3c59efa63dd1bae80471754b1497730e8
lukaszcichon48 <lukasz.cichon@sealights.io>
2022-05-18 14:59:45 +0200

    Added deployment files

---
+++ src/pack.sh
@@ -0,0 +1,3 @@
+﻿docker build -t microservices-demo-cartservice .
+docker tag microservices-demo-cartservice:latest 159616352881.dkr.ecr.eu-west-1.amazonaws.com/microservices-demo-cartservice:latest
+docker push 159616352881.dkr.ecr.eu-west-1.amazonaws.com/microservices-demo-cartservice:latest
