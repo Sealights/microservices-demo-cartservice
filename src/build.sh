d490719d492ecb273989302e79391543e3529a94
lukaszcichon48 <lukasz.cichon@sealights.io>
2022-05-18 16:56:33 +0200

    corrected script

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,4 @@
 ﻿$ bash file
+sudo rm -rf cartservice
 dotnet restore cartservice.csproj -r linux-musl-x64
+sudo dotnet publish cartservice.csproj -p:PublishSingleFile=true -r linux-musl-x64 --self-contained true -p:PublishTrimmed=True -p:TrimMode=Link -c release -o cartservice --no-restore
