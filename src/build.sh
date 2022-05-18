4516fea77de9a22e217d102ef55e542cb371ae12
lukaszcichon48 <lukasz.cichon@sealights.io>
2022-05-18 16:46:28 +0200

    Corrected path

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 ﻿$ bash file
 dotnet restore cartservice.csproj -r linux-musl-x64
-sudo dotnet publish cartservice.csproj -p:PublishSingleFile=true -r linux-musl-x64 --self-contained true -p:PublishTrimmed=True -p:TrimMode=Link -c release -o /cartservice --no-restore
+sudo dotnet publish cartservice.csproj -p:PublishSingleFile=true -r linux-musl-x64 --self-contained true -p:PublishTrimmed=True -p:TrimMode=Link -c release -o cartservice --no-restore
