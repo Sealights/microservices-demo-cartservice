f6ce574c2650fd104e3174eca5ff1f4599048bcc
lukaszcichon48 <lukasz.cichon@sealights.io>
2022-05-18 16:00:14 +0200

    Configuration changed

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,4 @@
 ﻿$ bash file
+dotnet restore cartservice.csproj -r linux-musl-x64
 dotnet build
+dotnet publish cartservice.csproj -p:PublishSingleFile=true -r linux-musl-x64 --self-contained true -p:PublishTrimmed=True -p:TrimMode=Link -c release -o /cartservice --no-restore
